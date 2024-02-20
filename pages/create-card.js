a528bc9d2c613e6e5c0582e728e07856d7428a53
amine <aminecheriguifr@gmail.com>
2024-02-20 14:14:44 +0100

    create cards

---
+++ pages/create-card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardCreationForm from '../components/CardCreationForm'; 
+
+const CreateCardPage = () => {
+  return (
+    <div>
+      <h1>Create a New Card</h1>
+      <CardCreationForm />
+    </div>
+  );
+};
+
+export default CreateCardPage;
