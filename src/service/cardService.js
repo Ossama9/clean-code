6b1f7e6b8564dd006c16b000dffbcc361a79cdb2
Ossama9 <oussama.dahbi98@gmail.com>
2024-02-23 18:18:37 +0100

    add promoteCardIfCorrect & getCardsToReview

--- src/service/cardService.js
+++ src/service/cardService.js
@@ -2,24 +2,56 @@ export default class CardService {
     constructor(cardRepo) {
         this.cardRepo = cardRepo;
     }
+    async getAllCards(tags = []) {
+        return this.cardRepo.getCardsByTags(tags);
+    }
 
     async promoteCardIfCorrect(cardId, isCorrect) {
         const card = await this.cardRepo.getCardById(cardId);
-        if (!card) {
-            throw new Error('Card not found');
-        }
-
         if (isCorrect) {
             if (card.category === 'SEVENTH') {
-                await this.cardRepo.removeCard(cardId); // La carte ne sera plus proposée
+                await this.cardRepo.removeCard(cardId);
             } else {
                 const nextCategory = this.getNextCategory(card.category);
-                await this.cardRepo.updateCardCategory(cardId, nextCategory);
+                await this.cardRepo.updateCard(cardId, {category: nextCategory, lastUpdated: Date.now()});
             }
         } else {
-            await this.cardRepo.resetCardCategory(cardId); // En cas de réponse incorrecte, retour à la catégorie 'FIRST'
+            await this.cardRepo.updateCard(cardId, {category: 'FIRST', lastUpdated: Date.now()});
         }
     }
+
+    async getCardsToReview(date) {
+        const allCards = await this.cardRepo.getCards();
+        const quizzDate = date ? new Date(date) : new Date();
+        quizzDate.setUTCHours(0, 0, 0, 0);
+        return allCards.filter(card => {
+            const lastUpdated = new Date(card.lastUpdated);
+            lastUpdated.setUTCHours(0, 0, 0, 0);
+
+            const daysUntilNextReview = this.getDaysUntilNextReview(card.category);
+            const nextReviewDate = new Date(lastUpdated);
+            if (card.category !== 'FIRST') {
+                nextReviewDate.setUTCDate(nextReviewDate.getUTCDate() + daysUntilNextReview);
+            }
+            return nextReviewDate <= quizzDate;
+        });
+
+    }
+
+
+    getDaysUntilNextReview(category) {
+        const intervals = {
+            'FIRST': 1,
+            'SECOND': 2,
+            'THIRD': 4,
+            'FOURTH': 8,
+            'FIFTH': 16,
+            'SIXTH': 32,
+            'SEVENTH': 64
+        };
+        return intervals[category] || 0;
+    }
+
 
     getNextCategory(currentCategory) {
         const categories = ['FIRST', 'SECOND', 'THIRD', 'FOURTH', 'FIFTH', 'SIXTH', 'SEVENTH'];
